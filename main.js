5cda0aa6c5c13e44caee60d013e7c98fac55908e
Purushottam Khedre <purushottamkhedre123@gmail.com>
2023-04-21 16:57:42 +0000

    modify server file

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 const express = require("express")
 const app = express();
+let count=0;
 
 app.get("/api/ping", (req,res)=>{
+    let {path, ip} = req;
+    console.log(count, ". ", path, ip);
+    count++;
     res.json({success: true});
 })
 
