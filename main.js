d5e0395de40ab8536735f2c01a914253d5848d7b
Purushottam Khedre <purushottamkhedre123@gmail.com>
2023-04-21 10:58:51 +0000

    basic server

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log("purushottam");
+const express = require("express")
+const app = express();
+
+app.get("/api/ping", (req,res)=>{
+    res.json({success: true});
+})
+
+
+app.listen(3000, ()=>{
+    console.log("server started")
+})
